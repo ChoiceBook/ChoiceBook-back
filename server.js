3313878113c31ac4f9d1ea6a355052eb2bd71581
wlsdnqqq17 <wlsdnqqq17@gmail.com>
2024-07-20 22:04:40 +0900

    ✨ <feat> 3 api add

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const pool = require('./config/database');
 
 const authRoutes = require('./routes/auth')
 //const plotRoutes = require('./routes/plot')
@@ -16,11 +17,57 @@ app.use(express.json());
 //app.options('*'.ors());
 
 //라우트 설정
-app.use('/api/auth', authRoutes)
+app.use('/api/auth', authRoutes);
 //app.use('api/plot', plotRoutes)
 
 //서버 시작
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
-})
+});
+
+app.get('/api/users/:user_id/categories/:category_id/plots', async (req, res) => {
+  const { user_id, category_id } = req.params;
+  try {
+      const [rows] = await pool.query(
+          `SELECT Plots.*
+           FROM Plots
+           JOIN UserPlayedPlots ON Plots.plot_id = UserPlayedPlots.plot_id
+           JOIN PlotCategories ON Plots.plot_id = PlotCategories.plot_id
+           WHERE UserPlayedPlots.user_id = ? AND PlotCategories.category_id = ?`,
+          [user_id, category_id]
+      );
+      res.json(rows);
+  } catch (error) {
+      console.error('Error fetching plots:', error);
+      res.status(500).json({ message: '서버 오류가 발생했습니다.' });
+  }
+});
+
+app.get('/api/plots/:plot_id/users/:user_id/ranks', async (req, res) => {
+  const { plot_id, user_id } = req.params;
+  try {
+      const [rows] = await pool.query(
+        'SELECT rank_id, plot_id, user_id, item_id, rank_value, ranked_at FROM Ranks WHERE plot_id = ? AND user_id = ? ORDER BY rank_value',          [plot_id, user_id]
+      );
+      res.json(rows);
+  } catch (error) {
+      console.error('Error fetching ranks:', error);
+      res.status(500).json({ message: '서버 오류가 발생했습니다.' });
+  }
+});
+
+app.get('/api/items/:item_id', async (req, res) => {
+  const { item_id } = req.params;
+  try {
+      const [rows] = await pool.query(
+          'SELECT * FROM Items WHERE item_id = ?',
+          [item_id]
+      );
+      res.json(rows);
+  } catch (error) {
+      console.error('Error fetching item:', error);
+      res.status(500).json({ message: '서버 오류가 발생했습니다.' });
+  }
+});
+
 
