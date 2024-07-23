6790fe69f1753c9adc8b06ea1f93ca74b363b006
wlsdnqqq17 <wlsdnqqq17@gmail.com>
2024-07-23 15:55:31 +0000

    ⚡️ image upload

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 const path = require('path');
 const pool = require('./config/database');
+const multer = require('multer');
 
 const authRoutes = require('./routes/auth');
 const plotRoutes = require('./routes/plots');
@@ -31,6 +32,24 @@ app.use((req, res, next) => {
 });
 
 app.options('*', cors()); // 올바른 CORS 설정
+
+// 파일 업로드를 위한 Multer 설정
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
+    const extension = path.extname(file.originalname);
+    const basename = req.body.title;
+    cb(null, `${basename}${extension}`);
+  }
+});
+const upload = multer({ storage: storage });
+
+// 이미지 업로드 라우트
+app.post('/api/upload', upload.single('image'), (req, res) => {
+  res.json({ message: 'File uploaded successfully', file: req.file });
+});
 
 // 라우트 설정
 app.use('/api/auth', authRoutes);
